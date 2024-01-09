63bdc8d9f8ef862ffefc91a4de5d6eca974c3de0
EONRaider <agsilva@tutanota.com>
2024-01-09 10:21:49 -0300

    Refactor code in OOP paradigm

--- public/js/results.js
+++ public/js/results.js
@@ -1,10 +1,24 @@
 /* eslint-disable no-undef */
 
-const currentRating = localStorage.getItem('currentRating');
-const totalScore = localStorage.getItem('totalScore');
-
-if ((currentRating !== 'null') && (totalScore !== 'null')) {
-  document.querySelector('.results__score').innerHTML = `You selected ${currentRating} out of ${totalScore}`;
-} else {
-  document.querySelector('.results__score').innerHTML = 'You selected no ratings yet!';
+function ResultsComponent() {
+  this.scoreElement = document.querySelector('.results__score');
 }
+
+ResultsComponent.prototype = {
+  get currentRating() {
+    return localStorage.getItem('currentRating');
+  },
+  get totalScore() {
+    return localStorage.getItem('totalScore');
+  },
+};
+
+ResultsComponent.prototype.renderResults = function renderResults() {
+  if ((this.currentRating !== 'null') && (this.totalScore !== 'null')) {
+    this.scoreElement.innerHTML = `You selected ${this.currentRating} out of ${this.totalScore}`;
+  } else {
+    this.scoreElement.innerHTML = 'You selected no ratings yet!';
+  }
+};
+
+new ResultsComponent().renderResults();
